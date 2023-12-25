9c71f3376f0b737cf54587fafbc2bf7a2235281f
niniarob <robakidzenino89@gmail.com>
2023-12-25 14:17:30 +0400

    add baner section, image,icons,h and p text, buttons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,99 @@
-import React from 'react';
+import React from "react";
+import Image from "../assets/avatar.png";
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div className='section' id='banner'>Banner</div>;
+  return (
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[50px] font-bold leading-[0.8] lg:text-[90px]"
+            >
+              NINO <span>ROBAKIDZE</span>{" "}
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <span className="text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={["Developer", 2000, "Design", 2000, "Youtuber", 2000]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              The error messages you're seeing indicate that there are missing
+              dependencies in your React project. Specifically, it seems that
+              the modules 'react-icons/bi', 'react-icons/bs', and 'react-scroll'
+              cannot be resolved.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg"> Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="#">
+                <FaYoutube />
+              </a>
+              <a href="#">
+                <FaDribbble />
+              </a>
+              <a href="#">
+                <FaGithub />
+              </a>
+            </motion.div>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1 mx-w-[320px] lg:max-w-[500px]"
+          >
+            <img className="rounded-full" src={Image} alt="" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,31 +12,47 @@ const Nav = () => {
         {/* nav inner */}
         <div className="w-full bg-black/20 h-[96px] backbrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50 ">
           <Link
-            to="banner"
+            to="home"
+            activeClass="active"
+            smooth={true}
+            spy={true}
+            offset={-200}
             className="cursor-pointer w-[60px] flex items-center justify-center"
           >
             <BiHomeAlt />
           </Link>
           <Link
             to="about"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] flex items-center justify-center"
           >
             <BiUser />
           </Link>
           <Link
             to="services"
+            smooth={true}
+            spy={true}
+            activeClass="active"
             className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
           >
             <BsClipboardData />
           </Link>
           <Link
             to="work"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] flex items-center justify-center"
           >
             <BsBriefcase />
           </Link>
           <Link
             to="contact"
+            activeClass="active"
+            smooth={true}
+            spy={true}
             className="cursor-pointer w-[60px] flex items-center justify-center"
           >
             <BsChatSquareText />
